79d93e0f9ce28baa3eafaacbca2f1f57bb5ce4ad
thanhnamcoder <nguyenthanhnamnv996@gmail.com>
2023-08-22 23:01:18 +0700

    sqlite3

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from fastapi import FastAPI, HTTPException
-import mysql.connector
-
+import sqlite3
 
 app = FastAPI()
 
 def get_db_connection():
-    db_config = {
-        'host': '103.97.126.24',
-        'user': 'beohbrrl_financial_freedom',
-        'password': 'nguyen2004nam',
-        'database': 'beohbrrl_financial_freedom',
-        'charset': 'utf8mb4',
-       'collation': 'utf8mb4_unicode_ci'
-    }
-    conn = mysql.connector.connect(**db_config)
+    conn = sqlite3.connect("data.db")  # Đổi tên file database tùy theo bạn
     return conn
 
 def close_db_connection(conn):
@@ -41,7 +32,7 @@ def delete_data(data_id: int):
     conn = get_db_connection()
     cursor = conn.cursor()
     
-    query = "DELETE FROM order_management WHERE id = %s"
+    query = "DELETE FROM order_management WHERE id = ?"
     cursor.execute(query, (data_id,))
 
     conn.commit()
@@ -56,7 +47,7 @@ def add_data(data: dict):
     cursor = conn.cursor()
 
     try:
-        query = "INSERT INTO order_management (product_name, date, must_have, nice_to_have, wasted) VALUES (%s, %s, %s, %s, %s)"
+        query = "INSERT INTO order_management (product_name, date, must_have, nice_to_have, wasted) VALUES (?, ?, ?, ?, ?)"
         values = (data['product_name'], data['date'], data['must_have'], data['nice_to_have'], data['wasted'])
         cursor.execute(query, values)
 
@@ -65,11 +56,10 @@ def add_data(data: dict):
         close_db_connection(conn)
 
         return {"message": "Data added successfully"}
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         print("Database Error:", err)
         raise HTTPException(status_code=500, detail="Error adding data to the database")
 
-        
 @app.post("/create_table")
 def create_table(table_info: dict):
     conn = get_db_connection()
@@ -80,13 +70,13 @@ def create_table(table_info: dict):
         columns = table_info["columns"]
 
         column_definitions = ", ".join([f"{col['name']} {col['type']}" for col in columns])
-        query = f"CREATE TABLE {table_name} ({column_definitions})"
+        query = f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, {column_definitions})"
         cursor.execute(query)
 
         # Add COLLATE for specific columns
         for col in columns:
             if col['name'] == 'product_name' or col['name'] == 'date':
-                alter_query = f"ALTER TABLE {table_name} MODIFY COLUMN {col['name']} {col['type']} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+                alter_query = f"ALTER TABLE {table_name} ALTER COLUMN {col['name']} SET DATA TYPE {col['type']} COLLATE utf8mb4_unicode_ci"
                 cursor.execute(alter_query)
 
         # Commit the table creation
@@ -110,11 +100,12 @@ def create_table(table_info: dict):
         close_db_connection(conn)
 
         return {"message": f"Table '{table_name}' created, data transferred, and old data cleared successfully"}
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         cursor.close()
         close_db_connection(conn)
         print("Error creating table:", err)
         raise HTTPException(status_code=500, detail=f"Error creating table: {err}")
+
 
 
 
@@ -137,7 +128,7 @@ def get_tables():
         conn.close()
 
         return {"tables": table_names}
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         print("Database Error:", err)
         raise HTTPException(status_code=500, detail="Error fetching tables from the database")
 
@@ -155,7 +146,7 @@ def delete_table(table_name: str):
         close_db_connection(conn)
 
         return {"message": f"Table '{table_name}' deleted successfully"}
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         print("Error deleting table:", err)
         cursor.close()
         close_db_connection(conn)
@@ -178,7 +169,7 @@ def get_table_data(table_name: str):
         close_db_connection(conn)
 
         return data
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         cursor.close()
         close_db_connection(conn)
         print("Database Error:", err)
@@ -190,7 +181,7 @@ def check_connection():
         conn = get_db_connection()
         close_db_connection(conn)
         return {"message": "Connected to the database"}
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         return {"message": "Failed to connect to the database"}
     
 @app.get("/get_all_tables_and_data")
@@ -218,7 +209,7 @@ def get_all_tables_and_data():
         close_db_connection(conn)
 
         return all_data
-    except mysql.connector.Error as err:
+    except sqlite3.Error as err:
         cursor.close()
         close_db_connection(conn)
         print("Database Error:", err)
